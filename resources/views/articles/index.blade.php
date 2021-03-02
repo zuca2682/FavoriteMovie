87ad70e85f34866c5e4fed90604106263c14dec5
zuca <2682zucazuca@gmail.com>
2021-03-02 21:00:30 +0900

    記事一覧へのダミーデータの表示

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -4,28 +4,30 @@
 
 @section('content')
   <div class="container">
-    <div class="card mt-3">
-      <div class="card-body d-flex flex-row">
-        <i class="fas fa-user-circle fa-3x mr-1"></i>
-        <div>
-          <div class="font-weight-bold">
-            ユーザー名
-          </div>
-          <div class="font-weight-lighter">
-            2021/3/2 20:00
+    @foreach($articles as $article)
+      <div class="card mt-3">
+        <div class="card-body d-flex flex-row">
+          <i class="fas fa-user-circle fa-3x mr-1"></i>
+          <div>
+            <div class="font-weight-bold">
+              {{ $article->user->name }}
+            </div>
+            <div class="font-weight-lighter">
+              {{ $article->created_at->format('Y/m/d H:i') }}
+            </div>
           </div>
         </div>
-      </div>
-      <div class="card-body pt-0 pb-2">
-        <h3 class="h4 card-title">
-          記事タイトル
-        </h3>
-        <div class="card-text">
-          記事本文
+        <div class="card-body pt-0 pb-2">
+          <h3 class="h4 card-title">
+            {{ $article->title }}
+          </h3>
+          <div class="card-text">
+            {!! nl2br(e( $article->body ))!!}
+          </div>
+          <!-- 投稿した画像を表示 -->
+          <img src="https://www.ghibli.jp/gallery/kazetachinu043.jpg" class="img-fluid shadow-2-strong" alt=""/>
         </div>
-        <!-- 投稿した画像を表示 -->
-        <img src="https://www.ghibli.jp/gallery/kazetachinu043.jpg" class="img-fluid shadow-2-strong" alt=""/>
       </div>
-    </div>
+    @endforeach
   </div>
 @endsection
