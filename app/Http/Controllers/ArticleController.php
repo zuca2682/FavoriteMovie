f09cfdbb2a6cfcf0f1e25e22b2458be7c3e821c4
zuca <2682zucazuca@gmail.com>
2021-03-08 17:30:32 +0900

    コントローラーを編集
    createのビューを表示する

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,4 +12,10 @@ class ArticleController extends Controller
         $articles = Article::all()->sortByDesc('created_at');
         return view('articles.index', ['articles' => $articles]);
     }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
 }
