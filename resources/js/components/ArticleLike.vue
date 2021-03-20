a476a39beaef459b84b78e5c6e64d74d68fa4b65
zuca <2682zucazuca@gmail.com>
2021-03-20 21:31:58 +0900

    Vueコンポーネントの編集
    いいねのカウントが正常に表示されたことを確認

--- resources/js/components/ArticleLike.vue
+++ resources/js/components/ArticleLike.vue
@@ -8,7 +8,7 @@
       :class="{'red-text':this.isLikedBy}"
       />
     </button>
-    10
+    {{ countLikes }}
   </div>
 </template>
 <script>
@@ -18,10 +18,15 @@
         type: Boolean,
         default: false,
       },
+      initialCountLikes: {
+        type: Number,
+        default: 0,
+      },
     },
     data() {
       return {
         isLikedBy: this.initialIsLikedBy,
+        countLikes: this.initialCountLikes,
       }
     },
   }
