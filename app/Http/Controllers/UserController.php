823f79eaa74292c1cbba3197cdf4744faef6b197
zuca <2682zucazuca@gmail.com>
2021-03-24 14:52:14 +0900

    アクションメソッドの追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function show(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
 }
